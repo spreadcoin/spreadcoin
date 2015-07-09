6a050b68738291426c299ddeae6be3bff4b2d5db
georgem <info@cryptoons.com>
2015-07-09 20:47:41 +0200

    Initial Setup

---
+++ README.md
@@ -0,0 +1,24 @@
+SpreadCoin integration/staging tree
+================================
+
+http://spreadcoin.net
+
+Copyright (c) 2009-2014 The developers
+
+What is SpreadCoin?
+----------------
+
+SpreadCoin is a new cryptocurrency which is more decentralized than Bitcoin. It prevents centralization of hashing power in pools, which is one of the main concerns of Bitcoin security. SpreadCoin was fairly launched with no premine.
+ - Algorithm: SpreadX11
+ - Block generation: 1 minute
+ - Difficulty retargets: every block based on last 360 blocks
+ - Reward starts at approx. 6.66 coins per block
+ - Block reward is smoothly halved every 4 years
+ - Total supply: 20 mln coins
+ - No year 2106 problem
+
+License
+-------
+
+SpreadCoin is released under the terms of the MIT license. See `COPYING` for more
+information or see http://opensource.org/licenses/MIT.
---
+++ src/qt/splashscreen.cpp
@@ -0,0 +1,69 @@
+#include "splashscreen.h"
+#include "clientversion.h"
+#include "util.h"
+
+#undef loop /* ugh, remove this when the #define loop is gone from util.h */
+#include <QApplication>
+
+#include <QDesktopWidget>
+
+SplashScreen::SplashScreen(QWidget *parent) :
+    QWidget(parent)
+{
+
+    QRect rec = QApplication::desktop()->screenGeometry();
+
+    int screenWidth = rec.width();
+    int screenHeight = rec.height();
+
+    this->setWindowFlags(Qt::FramelessWindowHint);
+    this->setGeometry(0,screenHeight/2-150,screenWidth,300);
+
+
+    QPixmap bgPixmap(screenWidth,300);
+
+    QLinearGradient bgGradient(QPointF(0, 0), QPointF(screenWidth, 0));
+    bgGradient.setColorAt(0, QColor("#6c3d94"));
+    bgGradient.setColorAt(1, QColor("#a13469"));
+    //#3c3c3b
+
+    QRect rect_linear(0,0,screenWidth,300);
+
+    QPainter *painter = new QPainter(&bgPixmap);
+    painter->fillRect(rect_linear, bgGradient);
+
+    painter->end();
+
+    bg = new QLabel(this);
+    bg->setPixmap(bgPixmap);
+
+
+    bg->setGeometry(0,0,screenWidth,300);
+
+    splashImage = new QLabel(this);
+    QPixmap newPixmap;
+    if(GetBoolArg("-testnet")) {
+        newPixmap.load(":/images/splash_testnet");
+    }
+    else {
+        newPixmap.load(":/images/splash");
+    }
+
+
+    splashImage->setPixmap(newPixmap);
+    splashImage->move(screenWidth/2-567/2,50);
+
+    QFont largeFont; largeFont.setPixelSize(20);
+
+    label = new QLabel(this);
+    label->setStyleSheet("QLabel { color: #FFFFFF; }");
+    label->setFont(largeFont);
+    label->setText("...");
+    label->setFixedSize(1000,30);
+    label->move(screenWidth/2-108,260);
+
+}
+
+SplashScreen::~SplashScreen()
+{
+}
